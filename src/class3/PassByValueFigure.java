409bef2e1df5d61b5893bb6d13eac1fc3a2cadd4
Iris <148431646+iris915520@users.noreply.github.com>
2025-07-08 21:45:54 +0300

    add walrus

--- src/class3/PassByValueFigure.java
+++ src/class3/PassByValueFigure.java
@@ -14,4 +14,14 @@ public class PassByValueFigure {
         W.weight = W.weight - 100;
         x = x - 5;
     }
+
+    public static class Walrus {
+        public int weight;
+        public double tuskSize;
+
+        public Walrus(int w, double ts) {
+            weight = w;
+            tuskSize = ts;
+        }
+    }
 }
