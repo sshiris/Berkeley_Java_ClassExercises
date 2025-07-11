6163ebe24711f7531bfcc3e17e251e3e6e53f342
Iris <148431646+iris915520@users.noreply.github.com>
2025-07-11 09:56:00 +0300

    add size and addlast function

--- src/class3/SLList.java
+++ src/class3/SLList.java
@@ -3,6 +3,16 @@ package class3;
 public class SLList {
     public IntNode first;
 
+    public class IntNode {
+        public int first;
+        public IntNode rest;
+
+        public IntNode(int first, IntNode rest) {
+            this.first = first;
+            this.rest = rest;
+        }
+    }
+
     public SLList(int x) {
         first = new IntNode(x, null);
     }
@@ -15,10 +25,35 @@ public class SLList {
         return first.first;
     }
 
+    private static int size(IntNode p) {
+        if (p.rest == null) {
+            return 1;
+        }
+        return 1 + size(p.rest);
+    }
+
+    public int size() {
+        return size(first);
+    }
+
+    public void addLast(int x) {
+        IntNode p = first;
+
+        while (p.rest != null) {
+            p = p.rest;
+        }
+
+        p.rest = new IntNode(x, null);
+
+    }
+
     public static void main(String[] args) {
         SLList L = new SLList(15);
         L.addFirst(10);
         L.addFirst(5);
+        L.addLast(25);
+        System.out.println(L.size());
         System.out.println(L.getFirst());
+        System.out.println(L.first);
     }
 }
